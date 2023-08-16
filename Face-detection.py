74ee02a8fa6022e8488e5ebe742a4893070d8ac5
chochey <30965960+chochey@users.noreply.github.com>
2023-08-16 13:20:59 -0400

    fixing structure

--- Face-detection.py
+++ Face-detection.py
@@ -4,15 +4,15 @@ import numpy as np
 from PIL import Image
 import pytesseract
 import logging
+import os
+
+# Define the keyword to search for
+wordToSearch = "Mark"
 
 # Load the pre-trained face cascade classifier
 face_cascade = cv.CascadeClassifier(
     "./haarcascade-files/haarcascade_frontalface_default.xml"
 )
-
-# the zip name and keyword for image search
-wordToSearch = "Mark"
-filename = "images.zip"
 
 
 # Function to find faces and clip them from an image
@@ -31,12 +31,65 @@ def clip_faces(image_data):
         return None, []
 
 
-# Open the ZIP file in read mode
-with zipfile.ZipFile("./zip-Files/" + filename, "r") as zip_ref:
-    image_files = [name for name in zip_ref.namelist() if name.lower().endswith(".png")]
+# Set the directory where zip files and images are located
+input_dir = "./input_data/"
+output_dir = "./output_data/"
 
-    for image_file in image_files:
-        with zip_ref.open(image_file) as img_file:
+# Create the output directory if it doesn't exist
+if not os.path.exists(output_dir):
+    os.makedirs(output_dir)
+
+# List all the zip files in the directory
+zip_files = [file for file in os.listdir(input_dir) if file.lower().endswith(".zip")]
+
+# Process images within zip files
+for zip_file in zip_files:
+    with zipfile.ZipFile(os.path.join(input_dir, zip_file), "r") as zip_ref:
+        for zip_info in zip_ref.infolist():
+            if zip_info.is_dir():
+                continue
+
+            print(f"Processing image file: {zip_info.filename}")
+
+            image_data = zip_ref.read(zip_info.filename)
+
+            img, faces = clip_faces(image_data)
+
+            if img is None:
+                continue
+
+            text = pytesseract.image_to_string(img, config="--psm 1 --oem 3")
+
+            if wordToSearch in text:
+                if len(faces) > 0:
+                    img_with_faces = img.copy()
+
+                    # Create a horizontal list of faces
+                    face_list = []
+                    for i, (x, y, w, h) in enumerate(faces):
+                        face_roi = img[y : y + h, x : x + w]
+                        resized_face = cv.resize(face_roi, (100, 100))
+                        face_list.append(resized_face)
+
+                    combined_faces = np.hstack(face_list)
+
+                    thumbnail_filename = f"combined_faces_{zip_info.filename}"
+                    output_path = os.path.join(output_dir, thumbnail_filename)
+                    cv.imwrite(output_path, combined_faces)
+
+                    print(f"Faces found in '{zip_info.filename}': {len(faces)}")
+                else:
+                    print(
+                        f"'{wordToSearch}' was found in the file '{zip_info.filename}' but no faces were detected."
+                    )
+
+# Process images outside zip files
+for image_file in os.listdir(input_dir):
+    if image_file.lower().endswith((".png", ".jpg", ".jpeg")):
+        print(f"Processing image file: {image_file}")
+
+        image_path = os.path.join(input_dir, image_file)
+        with open(image_path, "rb") as img_file:
             image_data = img_file.read()
 
         img, faces = clip_faces(image_data)
@@ -60,10 +113,13 @@ with zipfile.ZipFile("./zip-Files/" + filename, "r") as zip_ref:
                 combined_faces = np.hstack(face_list)
 
                 thumbnail_filename = f"combined_faces_{image_file}"
-                cv.imwrite(thumbnail_filename, combined_faces)
+                output_path = os.path.join(output_dir, thumbnail_filename)
+                cv.imwrite(output_path, combined_faces)
 
                 print(f"Faces found in '{image_file}': {len(faces)}")
             else:
                 print(
                     f"'{wordToSearch}' was found in the file '{image_file}' but no faces were detected."
                 )
+
+print("Program Completed")
